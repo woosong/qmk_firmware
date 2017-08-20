d1383aa3046731bd5cbdb0503599e450cda834b7
Woosong Choi <woosong@bawi.org>
2017-08-19 23:18:38 -0400

    minor tweak, put layer switches back where they were

--- keyboards/orthodox/keymaps/faux-kinesis/keymap.c
+++ keyboards/orthodox/keymaps/faux-kinesis/keymap.c
@@ -49,27 +49,24 @@ enum custom_keycodes {
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-#define LG_BSPC MT(MOD_LGUI, KC_BSPC)
-#define RC__SPC MT(MOD_RCTL, KC_SPC)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   LOWER,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LCTL, XXXXXXX, KC_LALT,          KC_RALT, XXXXXXX, KC_RGUI, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LG_BSPC, KC_ESC,  LOWER,            RAISE,   KC_ENT,  RC__SPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,   KC_BSPC, KC_ESC,           KC_ENT,  KC_SPC,  RAISE,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT \
 ),
 
 [_LOWER] = KEYMAP( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                                KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_UNDS, \
-  _______, KC_INS,  KC_HOME, KC_PGUP, _______, _______, _______, XXXXXXX, _______,          KC_RALT, XXXXXXX, KC_RGUI, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, _______, _______, \
-  KC_LSFT, KC_DEL,  KC_END,  KC_PGDN, _______, _______, _______, _______, _______,          _______, _______, KC_RCTL, _______, _______, _______, KC_LCBR,  KC_RCBR, KC_RSFT \
+  _______, KC_INS,  KC_HOME, KC_PGUP, _______, _______, _______, XXXXXXX, _______,          _______, XXXXXXX, _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, _______, KC_EQL, \
+  KC_LSFT, KC_DEL,  KC_END,  KC_PGDN, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, KC_LCBR,  KC_RCBR, KC_RSFT \
 ),
 
 [_RAISE] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCTL, XXXXXXX, KC_LALT,          _______, XXXXXXX, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PLUS, \
-  KC_LSFT, KC_F11,  KC_F12,  _______, _______, _______, KC_LGUI, _______, _______,          _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_RSFT \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, XXXXXXX, _______,          _______, XXXXXXX, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PLUS, \
+  KC_LSFT, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_RSFT \
 ),
 
 [_ADJUST] =  KEYMAP( \
